52829215727e5fbef985acf7e05a01c9d08a0486
user <xxxdarkknight74@swag.end>
2024-03-14 23:07:39 +0100

    ajout loader

--- src/app/components/landing/loader/loader.component.ts
+++ src/app/components/landing/loader/loader.component.ts
@@ -1,13 +1,25 @@
-import {AfterContentInit, AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
+import {NgIf} from "@angular/common";
+import {filter, Subject, takeUntil} from "rxjs";
+import {NavigationEnd, NavigationStart, Router} from "@angular/router";
 
 @Component({
   selector: 'app-loader',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.css'
+  styleUrl: './loader.component.css',
+  animations: [trigger('killComponent', [
+    transition(':leave',
+        animate('700ms 300ms ease',style({bottom:'-110%'}))
+      ),
+    ]),
+  ]
 })
-export class LoaderComponent implements AfterViewInit {
+export class LoaderComponent implements OnInit, AfterViewInit {
 
   @ViewChild('ball') ball!: ElementRef;
   @ViewChild('ballPath') ballPath!: ElementRef;
@@ -16,29 +28,40 @@ export class LoaderComponent implements AfterViewInit {
   distance : number = 1;
   totalLength : number = 0;
 
+  hideLogo : boolean = false;
+  stopAnimation : boolean = false;
+  killComponent : boolean = false;
 
-  animateBallLoader(){
-    if (this.distance > this.totalLength) {
-      this.distance = 0;
-    }
-    console.log((this.ballPath.nativeElement as SVGGeometryElement).getPointAtLength(this.distance))
+  loadingTime : number = 0;
+  loadingTimerInterval : any;
 
-    this.point = (this.ballPath.nativeElement as SVGGeometryElement).getPointAtLength(this.distance)
+  unSubscribeRouter$ : Subject<void> = new Subject<void>
 
-    this.ball.nativeElement.setAttribute('cx', this.point.x);
-    this.ball.nativeElement.setAttribute('cy', this.point.y);
+  constructor(private router: Router) { }
 
-/*    this.ball.nativeElement.style.left = `${this.point.x}px`;
-    this.ball.nativeElement.style.top = `${this.point.y}px`;*/
+  ngOnInit() {
 
-    this.distance+=1
+    // timer temps de visualisation du loader
+    setInterval(()=>{
+      this.loadingTime += .5
+    },500)
 
-    requestAnimationFrame(
-      ()=>{
-        this.animateBallLoader()
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd || event instanceof NavigationStart),
+      takeUntil(this.unSubscribeRouter$)
+    ).subscribe((event : any) => {
+      // qd la page est chargée
+      if (event instanceof NavigationEnd) {
+        //si le loader a été vu plus de 2 sec
+        if(this.loadingTime > 2){
+          this.fnPageLoaded();
+        }else{
+          setTimeout(()=>{
+            this.fnPageLoaded();
+          },2000)
+        }
       }
-    )
-
+    });
   }
 
   ngAfterViewInit () {
@@ -48,6 +71,41 @@ export class LoaderComponent implements AfterViewInit {
         this.animateBallLoader()
       })
     }
+  }
+
+  animateBallLoader(){
+    if(!this.killComponent){
+      this.point = this.ballPath.nativeElement.getPointAtLength(this.distance);
+
+      this.ball.nativeElement.setAttribute('cx', this.point.x.toString());
+      this.ball.nativeElement.setAttribute('cy', this.point.y.toString());
+
+      if(this.distance >= this.totalLength){
+        this.distance = 0
+      }
+      this.distance += 0.6;
+
+      requestAnimationFrame(() => {
+        this.animateBallLoader();
+      });
+    }
+  }
+
+  fnPageLoaded(){
+    clearInterval(this.loadingTimerInterval)
+    this.unSubscribeRouter$.next()
+    this.unSubscribeRouter$.complete()
+
+    this.hideLogo = true;
+
+    setTimeout(()=>{
+      this.killComponent = true
+      setTimeout(()=>{
+        this.stopAnimation = true
+      },500)
+    },500)
+
+
   }
 
 }
